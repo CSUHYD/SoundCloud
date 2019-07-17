0329171b08698be8409ff03d2c271918a515d53b
cheng.ma <cheng.ma@owitho.com>
2019-07-17 12:01:47 +0800

    ajax

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 import './AudioRecorder';
+import axios from "axios/index";
+
+const CLUSTER_COLORS = [];
 
 export default class App extends React.Component {
     constructor(props) {
@@ -32,7 +35,25 @@ export default class App extends React.Component {
         this.recorder.stopAndUpload();
     }
     uploadAll() {
-
+        const {audio} = this.state;
+        var fd = new FormData();
+        const len = audio.length;
+        for (let i = 0; i < len; i++) {
+            fd.append(`${i}`, audio[i].blob);
+        }
+        const arr = JSON.parse(JSON.stringify(this.state.audio));
+        axios.post('http://localhost:5000/projects', fd).then((res) => { //todo robizlab.com
+            if (res && res.data) {
+                console.log(res.data);//todo
+                Object.keys(res.data).forEach((key) => {
+                    arr[key].cluster = res.data[key].cluster;
+                    arr[key].coord = res.data[key].coord;
+                })
+                this.setState({
+                    audio: arr,
+                })
+            }
+        });
     }
     getRandomColor() {
         var letters = '0123456789ABCDEF';
@@ -62,6 +83,7 @@ export default class App extends React.Component {
                                         marginTop:'5px',
                                         width: '20px',
                                         height:'20px',
+                                        // if cluster exist, pick color[cluster]
                                         backgroundColor: `rgb(${item.coord[0]*255}, ${item.coord[1]*255}, 125)`,
                                         left: item.coord[0]*100+'%',
                                         top: item.coord[1]*100+'%',
@@ -93,7 +115,9 @@ export default class App extends React.Component {
                             ]
                         })
                     }} type="danger" size="small">结束录音并添加</button>
-                    <button onClick={()=>{}}>全部上传</button>
+                    <button onClick={()=>{
+                        this.uploadAll();
+                    }}>全部上传</button>
                 </div>
             </div>
         )
